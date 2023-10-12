36fca083568a5da4decc02f94ae08c30f3bf309e
Robin <robinp31@uw.eduL>
2023-10-11 22:50:33 -0600

    on variables

--- Class 1 Exercises.js
+++ Class 1 Exercises.js
@@ -5,19 +5,47 @@
 // 1. Pagliacci charges $16.99 for a 13” pizza and $19.99 for a 17” pizza.
 // What is the area for each of these pizzas?
 // (radius would be the listed size - i.e. 13" - divided by 2)
-
+const LARGE_PIZZA_DIAMETER = '17';
+const SMALL_PIZZA_DIAMETER = '13';
+console.log(`Question 1.`);
+let radiusLargePizza = LARGE_PIZZA_DIAMETER / 2;
+let areaLargePizza = Math.PI * Math.pow(radiusLargePizza,2);
+console.log(areaLargePizza,`area Large Pizza`)
+let radiusSmallPizza= SMALL_PIZZA_DIAMETER / 2;
+let areaSmallPizza = Math.PI * Math.pow(radiusSmallPizza,2);
+console.log(areaSmallPizza,`area Small Pizza`)
 
 // 2. What is the cost per square inch of each pizza?
-
+console.log(`Question 2.`);
+let smallPizzaCost = 16.99;
+let largePizzaCost = 19.99;
+console.log(`Small Pizza cost per inch`,smallPizzaCost/areaSmallPizza,
+`\nLarge Pizza cost per inch`, largePizzaCost/areaLargePizza);
 
 // 3. Using the Math object, put together a code snippet
 // that allows you to draw a random card with a value
 // between 1 and 13 (assume ace is 1, jack is 11…)
+console.log(`Question 3.`);
+let max = 13
+let min = 1
 
+const randomCard = Math.floor(min+Math.random() * max);
+
+console.log(randomCard)
 
 // 4. Draw 3 cards and use Math to determine the highest
 // card
-
+console.log(`Question 4.`);
+const randomCard1 = Math.floor(min+Math.random() * max);
+const randomCard2 = Math.floor(min+Math.random() * max);
+const randomCard3 = Math.floor(min+Math.random() * max);
+let isOne = randomCard1 >= randomCard2 && randomCard1>=randomCard3;
+let isTwo = randomCard2 >= randomCard3 && randomCard2>=randomCard1;
+let isThree = randomCard3 >= randomCard1 && randomCard3>=randomCard2;
+console.log("first is greatest?:", isOne)
+console.log("second is greatest?:", isTwo)
+console.log("third is greatest?:", isThree)
+console.log("firstCard", randomCard1,"secondCard",randomCard2,"thirdCard", randomCard3)
 
 /**
  * ADDRESS LINE
